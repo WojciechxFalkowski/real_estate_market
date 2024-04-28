2875e1c71218ba9e798c7f215cde567df90c6a8c
Wojtek Falkowski <wojciechxfalkowski@gmail.com>
2024-04-28 12:56:51 +0200

    add tiptap (better wyswig)

--- components/Tiptap.vue
+++ components/Tiptap.vue
@@ -101,7 +101,6 @@
   </div>
   <editor-content :editor="editor" class="my-4 bg-white" />
 
-  <!-- <button @click="saveData">save</button> -->
   <Button
     @click="saveData"
     class="block m-auto"
@@ -115,43 +114,61 @@
 </template>
 
 <script setup lang="ts">
-import { useEditor, Editor, EditorContent } from "@tiptap/vue-3";
+import { Editor, EditorContent } from "@tiptap/vue-3";
 import StarterKit from "@tiptap/starter-kit";
 import { Theme, ComponentType } from "@/components/Button/Button.props";
 import Underline from "@tiptap/extension-underline";
-defineProps({
-  modelValue: {
+const props = defineProps({
+  tiptapContent: {
     type: String,
     default: "",
   },
 });
 
 const emit = defineEmits<{
-  change: [id: number]; // named tuple syntax
-  update: [value: string];
+  submit: [value: string];
 }>();
+
 const isLoading = ref(false);
 const editor = ref<Editor>();
+
 onMounted(() => {
   editor.value = new Editor({
     extensions: [StarterKit, Underline],
-    content: `
-<p><strong>LUKSUSOWY APARTAMENT W UNIQUE TOWER WYKOŃCZONY POD KLUCZ</strong></p><p><strong><em>Trzy sypialnie | W pełni umeblowany | Wysokie piętro | Luksusowe wykończenie | Wysoki standard | Klimatyzacja</em></strong><br><br><strong>ROZKŁAD I STANDARD:</strong></p><p>Apartament składa się z:<br>* Przestronnego salonu z otwartą kuchnią i jadalnią,<br>* trzech sypialni,<br>* trzech łazienek,<br>* reprezentacyjnego holu z garderobą.<br></p><p>Apartament zaprojektowany przez architekta, jego głównymi zaletami jest wysoka jakość wykończenia oraz uniwersalny styl. Ponadczasowy design, idealnie skomponowane materiały, kolorystyka.&nbsp;Wyposażony został w klimatyzację, w pełni umeblowany i wyposażony w zastawę stołową, pościel, ręczniki, gotowy do wprowadzenia od zaraz.<br></p><p>Wykończenie z najwyższą dbałością o jakość.<br></p><p><strong>INWESTYCJA:</strong><br></p><p>Unique Tower to kompleks apartamentowców, zlokalizowany na bliskiej Woli, przy ul. Grzybowskiej 51. W budynku o 29 kondygnacjach poza apartamentami znajduje się szereg udogodnień- na parterze budynku mieści się przestrzeń usługowo-handlowa, najwyższej klasy restauracja, strefa SPA i fitness oraz kompleks konferencyjno-biznesowy.</p><p>Designerskie wnętrza części wspólnych i 5-gwiazdkowy standard apartamentów to gwarancja komfortu, prestiżu oraz najwyższej jakości.<br></p><p><strong>LOKALIZACJA:</strong></p><p>Ulica Grzybowska położona jest w samym sercu Warszawy.<br>Pieszy dystans do wszystkich środków komunikacji miejskiej, w tym stacji przesiadkowej metra M1/M2 Świętokrzyska.</p><p>Centralna lokalizacja wraz z doskonałą infrastrukturą handlowo – usługową dają duży komfort mieszkania, tuż obok znajdują się takie punkty usługowe jak: apteka, sklepy spożywcze, salon fryzjerko- kosmetyczny, banki. W pobliżu także: kina, teatry, Ogród Saski, sklepy, piekarnie, kawiarnie, biurowce, pięciogwiazdkowe hotele.</p><p>Dwa kroki do Dworca Centralnego i CH Złote Tarasy. Okolica pełna najbardziej znanych i cenionych w Warszawie restauracji.<br></p><p><strong>INFORMACJE DODATKOWE:</strong></p><p>* Wymaga jest kaucja.<br>* Miejsce parkingowe płatne dodatkowo oraz zaliczki na media. <br>* Do kwoty najmu należy doliczyć 23% podatku VAT.</p><p></p><p><strong>Serdecznie zapraszam na prezentację!</strong></p>
-      `,
+    content: props.tiptapContent,
+    editorProps: {
+      attributes: {
+        class: "min-h-96 border",
+      },
+    },
   });
 });
 
+watch(
+  () => props.tiptapContent,
+  (newContent) => {
+    if (editor.value) {
+      editor.value.commands.setContent(newContent);
+    }
+  }
+);
+
 const saveData = async () => {
-  isLoading.value = true;
-  await new Promise((resolve) => {
-    setTimeout(() => {
-      isLoading.value = false;
-      resolve("");
-    }, 1000);
-  });
+  if (!editor.value) {
+    return;
+  }
   console.log("saveData");
-  console.log(editor.value?.getHTML());
+  // console.log();
+  emit("submit", editor.value.getHTML());
+  // isLoading.value = true;
+  // await new Promise((resolve) => {
+  //   setTimeout(() => {
+  //     isLoading.value = false;
+  //     resolve("");
+  //   }, 1000);
+  // });
 };
+
 onBeforeUnmount(() => {
   if (!editor.value) {
     return;
@@ -173,7 +190,6 @@ onBeforeUnmount(() => {
     background: white;
     accent-color: black;
     &.is-active {
-      // font-weight: bold;
       background: black;
       color: #fff;
     }
