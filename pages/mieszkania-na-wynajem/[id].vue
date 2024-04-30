b87472222c4ac40d75f1bc1705e0ea3eb23faa40
Wojtek Falkowski <wojciechxfalkowski@gmail.com>
2024-04-30 12:58:20 +0200

    fix flat content styles

--- pages/mieszkania-na-wynajem/[id].vue
+++ pages/mieszkania-na-wynajem/[id].vue
@@ -11,14 +11,17 @@
       class="mt-5"
     />
 
-    <div class="mt-16" v-if="flatModel">
-      <h1 class="text-2xl text-gray-500">{{ flatModel.title }}</h1>
+    <div class="mt-8" v-if="flatModel">
+      <h1 class="text-lg uppercase text-gray-500">{{ flatModel.title }}</h1>
 
-      <p class="text-lg mt-4">{{ flatModel.description }}</p>
+      <p class="mt-4 font-bold">{{ flatModel.description }}</p>
     </div>
 
-    <div class="grid grid-cols-12">
-      <div class="long-content mt-8 col-span-12 lg:col-span-7 2xl:col-span-6" v-html.safe="flatModel?.tiptapHTML"></div>
+    <div class="grid grid-cols-12 mt-4">
+      <div
+        class="long-content col-span-12 lg:col-span-12 2xl:col-span-12"
+        v-html.safe="flatModel?.tiptapHTML"
+      ></div>
     </div>
   </section>
 </template>
@@ -29,3 +32,17 @@ const id = route.params.id;
 const { fetchFlat, flatModel } = useFlat();
 await fetchFlat(id);
 </script>
+
+<style lang="scss">
+.long-content {
+  font-size: 14px;
+  line-height: 23px;
+
+  ul {
+    padding-left: 16px;
+    li {
+      list-style-type: square;
+    }
+  }
+}
+</style>
