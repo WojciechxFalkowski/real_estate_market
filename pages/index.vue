927c3b52edffd46494c748222539f01afb94b4fa
Wojtek Falkowski <wojciechxfalkowski@gmail.com>
2024-05-18 22:00:21 +0200

    home carousel fetches images from api

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
     :animation="true"
   /> -->
   <!-- <div class="container m-auto"> -->
-  <HeroCarousel :pictures="pictures" :autoplay="2000" />
+  <HeroCarousel :pictures="homeCarouselImages" :autoplay="2000" />
 
   <div v-if="teamMembers.length" class="container mx-auto mt-4 lg:mt-16">
     <PageTitle :title="pageConfiguration?.title" class="mb-8 lg:mb-16" />
@@ -34,53 +34,6 @@
 </template>
 
 <script setup>
-// import Carousel from "@/components/Carousel.vue";
-// import Carousel2 from "@/components/Carousel2.vue";
-const pictures = [
-  // {
-  //   src: "https://flowbite.com/docs/images/carousel/carousel-1.svg",
-  //   alt: "Image 1",
-  // },
-  //   {
-  //     src: "https://flowbite.com/docs/images/carousel/carousel-2.svg",
-  //     alt: "Image 2",
-  //   },
-  //   {
-  //     src: "https://flowbite.com/docs/images/carousel/carousel-3.svg",
-  //     alt: "Image 3",
-  //   },
-  // {
-  //   srcset:
-  //     "https://api.cl.lirene.com/storage/2024/02/28/90x60px/lir130-banner-www-desktop-0286-1.p5cx.jpg 400w,https://api.cl.lirene.com/storage/2024/02/28/580xpx/lir130-banner-www-desktop-0286-1.qdbj.jpg 580w,https://api.cl.lirene.com/storage/2024/02/28/1180xpx/lir130-banner-www-desktop-0286-1.12pe.jpg 1180w,https://api.cl.lirene.com/storage/2024/02/28/1200xpx/lir130-banner-www-desktop-0286-1.sq48.jpg 1200w,https://api.cl.lirene.com/storage/2024/02/28/1920xpx/lir130-banner-www-desktop-0286-1.hu1j.jpg 1920w",
-  //   alt: "Image 1",
-  // },
-  // {
-  //   srcset:
-  //     "https://flowbite.com/docs/images/carousel/carousel-1.svg 600w, /images/carousel_1.jpeg 1024w",
-  //   alt: "Image 1",
-  // },
-  {
-    src: "/images/carousel_1.jpeg",
-    srcset: "/images/carousel_1.jpeg 600w,",
-    alt: "Image 1",
-  },
-  {
-    src: "/images/carousel_2.jpeg",
-    // srcset: "/images/carousel_2.jpeg 600w,",
-    alt: "Image 2",
-  },
-  {
-    src: "/images/carousel_3.jpeg",
-    // srcset: "/images/carousel_3.jpeg 600w,",
-    alt: "Image 3",
-  },
-  {
-    src: "/images/carousel_4.png",
-    // srcset: "/images/carousel_3.jpeg 600w,",
-    alt: "Image 4",
-  },
-];
-
 const { teamMembers, getTeamMembers } = useTeamMember();
 await getTeamMembers();
 
@@ -91,13 +44,29 @@ useHead({
   title: "wellrent-nieruchomosci.pl | Mieszkania na wynajem Warszawa",
   meta: [
     {
+      name: "description",
+      content: "Pośrednicy w obrocie nieruchomościami",
+    },
+    {
       property: "og:title",
       content: "wellrent-nieruchomosci.pl | Mieszkania na wynajem Warszawa",
     },
     {
-      property: "robots",
-      content: "index, follow",
+      property: "og:description",
+      content: "Pośrednicy w obrocie nieruchomościami",
     },
   ],
+});
+
+const { getHomeCarouselImages, carouselCollection } = useHomeCarouselImage();
+await getHomeCarouselImages();
+
+const homeCarouselImages = computed(() => {
+  return carouselCollection.value.map((homeCarouselImage) => {
+    return {
+      src: homeCarouselImage.url,
+      alt: "",
+    };
+  });
 });
 </script>
