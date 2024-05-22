c922b8cd82124692b72837365ac50b9e81ff4492
Wojtek Falkowski <wojciechxfalkowski@gmail.com>
2024-05-22 23:57:42 +0200

    increase size of chunks

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	vite: {
+		build: {
+			chunkSizeWarningLimit: 1000
+		}
+	},
 	modules: [
 		'@nuxtjs/tailwindcss',
 		'@vee-validate/nuxt',
