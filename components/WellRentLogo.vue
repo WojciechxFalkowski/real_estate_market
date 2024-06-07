cd27891922001db7eaf795ba62786a1efdc90693
Wojtek Falkowski <wojciechxfalkowski@gmail.com>
2024-06-07 14:28:00 +0200

    Track click event on logo

--- components/WellRentLogo.vue
+++ components/WellRentLogo.vue
@@ -1,10 +1,24 @@
 <template>
-  <!-- Logo and brand name -->
-  <RouterLink to="/" aria-label="Przekierowanie na stronę https://wellrent-nieruchomosci.pl">
+  <RouterLink
+    @click="handleClickLogo"
+    to="/"
+    aria-label="Przekierowanie na stronę https://wellrent-nieruchomosci.pl"
+  >
     <WellRent :style="{ width: '80px' }" />
   </RouterLink>
 </template>
 
 <script lang="ts" setup>
 import WellRent from "./icons/WellRent.vue";
+
+const props = defineProps({
+  componentPlacement: {
+    type: String,
+  },
+});
+const { trackClick } = useAnalytics();
+
+const handleClickLogo = () => {
+  trackClick(`logo`, `położenie: ${props.componentPlacement}`);
+};
 </script>
